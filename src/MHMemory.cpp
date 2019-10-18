77ebcd5747e203f4532f89363f981901dd6b33d8
Carlos Cortés <alcros33@gmail.com>
2019-10-17 21:27:17 -0500

    Added support for game version 167898

--- src/MHMemory.cpp
+++ src/MHMemory.cpp
@@ -192,7 +192,8 @@ std::map<std::string, SearchPattern> MH_Memory::Versions{
     {"167353", {BytesToInt({ 55, 193, 66, 1}), (0x068C) + 29}},
     {"167541", {BytesToInt({214, 199, 66, 1}), (0x06AC) + 29}},
     {"167796", {BytesToInt({ 87, 200, 66, 1}), (0x06AC) + 29}},
-    {"Latest", {BytesToInt({ 87, 200, 66, 1}), (0x06AC) + 29}}};
+    {"167898", {BytesToInt({103, 200, 66, 1}), (0x06AC) + 29}},
+    {"Latest", {BytesToInt({103, 200, 66, 1}), (0x06AC) + 29}}};
 
 // The distance between the address of the data of the character slots
 std::map<std::string, int> MH_Memory::CharSlotDist{
@@ -202,6 +203,7 @@ std::map<std::string, int> MH_Memory::CharSlotDist{
     {"167353", 1285888},
     {"167541", 1285920},
     {"167796", 1285920},
+    {"167898", 1285920},
     {"Latest", 1285920}};
 
 /// End MH_Memory Member definitions
