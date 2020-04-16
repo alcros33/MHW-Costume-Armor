011638b721e7ca88b39f898b59cd4c0b6c084cde
Carlos Cortés <alcros33@gmail.com>
2020-04-16 16:44:04 -0500

    Compatibility with version 410013

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [0.16.9] - 2020 - 04 - 16
+### Added
+- Compatibility with version 410013
+- Furious Rajang and Raging Brachydios
+
 ## [0.16.8] - 2020 - 03 - 29
 ### Added
 - Compatibility with version 408899
--- src/MHMemory.cpp
+++ src/MHMemory.cpp
@@ -186,7 +186,8 @@ std::map<QString, SearchPattern> MH_Memory::versions{
     {"404549", {BytesToUInt({148,   0, 67, 1}), (0x4B4) + 29}},
     {"406510", {BytesToUInt({188,  11, 67, 1}), (0x4B4) + 29}},
     {"408899", {BytesToUInt({204,  11, 67, 1}), (0x4B4) + 29}},
-    {"Latest", {BytesToUInt({204,  11, 67, 1}), (0x4B4) + 29}}
+    {"410013", {BytesToUInt({232,  16, 67, 1}), (0x4B4) + 29}},
+    {"Latest", {BytesToUInt({232,  16, 67, 1}), (0x4B4) + 29}},
 };
 
 // The distance between the address of the data of the character slots
@@ -205,6 +206,7 @@ std::map<QString, int> MH_Memory::charSlotDist{
     {"404549", 2615792},
     {"406510", 2615792},
     {"408899", 2615792},
+    {"410013", 2615792},
     {"Latest", 2615792},
 };
 
